a185dc51c717617dd82c10ffdfa1e1ea5a5b2f79
Wafa Saefulhaq <saefulhaqwafa@gmail.com>
2023-01-31 22:58:52 +0700

    added assets and created header

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,90 @@
-import './App.css'
+import { useState } from "react";
+
+function MenuMobile() {
+  const [showMobileMenu, setShowMobileMenu] = useState(false);
+
+  return (
+    <>
+      {/* Menu Mobile */}
+      {showMobileMenu && (
+        <nav className="h-full bg-red-600 w-full">
+          <ul className="text-neutral-dark-grayish-blue gap-8 border-debug">
+            <li>
+              <a href="/">Home</a>
+            </li>
+            <li>
+              <a href="#">New</a>
+            </li>
+            <li>
+              <a href="#">Popular</a>
+            </li>
+            <li>
+              <a href="#">Trending</a>
+            </li>
+            <li>
+              <a href="#">Categories</a>
+            </li>
+          </ul>
+        </nav>
+      )}
+
+      {/* Mobile Menu Button */}
+      <div className="md:hidden">
+        <button
+          className="w-12 h-12 flex items-center justify-center"
+          onClick={() => setShowMobileMenu(!showMobileMenu)}
+        >
+          {showMobileMenu ? (
+            <img src="/images/icon-menu-close.svg" alt="Close Menu" />
+          ) : (
+            <img src="/images/icon-menu.svg" alt="Open Menu" />
+          )}
+          <span className="sr-only">Menu Button</span>
+        </button>
+      </div>
+    </>
+  );
+}
 
 function App() {
   return (
-    <div className="App">
-       <h1 className="text-3xl font-bold underline">Hello world!</h1>
-       <h1 className="text-3xl font-bold underline">news homepage</h1>
-    </div>
-  )
+    <>
+      <header>
+        <div className="layout flex justify-between items-center">
+          <div>
+            <a href="/">
+              <img src="/images/logo.svg" alt="News Logo" />
+              <span className="sr-only">Logo</span>
+            </a>
+          </div>
+
+          <div className="hidden md:flex">
+            <nav>
+              <ul className="flex text-neutral-dark-grayish-blue gap-8 border-debug">
+                <li>
+                  <a href="/">Home</a>
+                </li>
+                <li>
+                  <a href="#">New</a>
+                </li>
+                <li>
+                  <a href="#">Popular</a>
+                </li>
+                <li>
+                  <a href="#">Trending</a>
+                </li>
+                <li>
+                  <a href="#">Categories</a>
+                </li>
+              </ul>
+            </nav>
+          </div>
+
+          <MenuMobile />
+        </div>
+      </header>
+    </>
+  );
 }
 
-export default App
+export default App;
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,11 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: {
+          "soft-orange": "#e9ab53",
+          "soft-red": "#f15e50",
+        },
+        neutral: {
+          "off-white": "#fffdfa",
+          "grayish-blue": "#c5c6ce",
+          "dark-grayish-blue": "#5d5f79",
+          "very-dark-blue": "#00001a",
+        },
+      },
+    },
   },
   plugins: [],
-}
+};

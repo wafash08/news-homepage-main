5da9f6d5b1be97f550c3649f5a2775654cb8a990
Wafa Saefulhaq <saefulhaqwafa@gmail.com>
2023-01-31 20:09:31 +0700

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="App">
+       <h1 className="text-3xl font-bold underline">Hello world!</h1>
+       <h1 className="text-3xl font-bold underline">news homepage</h1>
+    </div>
+  )
+}
+
+export default App

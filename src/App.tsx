9b2c304d9b136e3bf8a36eeda946fe0e7071444b
Wafa Saefulhaq <saefulhaqwafa@gmail.com>
2023-02-04 08:26:18 +0700

    setting up menu item component

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 
 function MenuButton({
   showMobileMenu,
@@ -13,9 +13,19 @@ function MenuButton({
       onClick={() => handleClick(!showMobileMenu)}
     >
       {showMobileMenu ? (
-        <img src="/images/icon-menu-close.svg" alt="Close Menu" />
+        <img
+          src="/images/icon-menu-close.svg"
+          alt="Close Menu"
+          width="32"
+          height="31"
+        />
       ) : (
-        <img src="/images/icon-menu.svg" alt="Open Menu" />
+        <img
+          src="/images/icon-menu.svg"
+          alt="Open Menu"
+          width="40"
+          height="17"
+        />
       )}
       <span className="sr-only">Menu Button</span>
     </button>
@@ -24,16 +34,39 @@ function MenuButton({
 
 function MenuItem({
   href,
+  size,
   children,
 }: {
   href: string;
+  size: "base" | "2xl";
   children: React.ReactNode;
 }) {
+  let fontSize = "";
+  let paddingLeft = "";
+
+  switch (size) {
+    case "base": {
+      fontSize = "text-base";
+      paddingLeft = "pl-0";
+      break;
+    }
+    case "2xl": {
+      fontSize = "text-2xl";
+      paddingLeft = "pl-5";
+      break;
+    }
+    default: {
+      fontSize = "text-base";
+      paddingLeft = "pl-0";
+      break;
+    }
+  }
+
   return (
     <li>
       <a
         href={href}
-        className="text-2xl w-full inline-flex pl-5 text-neutral-dark-grayish-blue transition-colors hover:text-primary-soft-red"
+        className={`${fontSize} w-full inline-flex ${paddingLeft} text-neutral-dark-grayish-blue transition-colors hover:text-primary-soft-red`}
       >
         {children}
       </a>
@@ -57,11 +90,21 @@ function MenuMobile({ showMobileMenu }: { showMobileMenu: boolean }) {
       <div className="w-4/5 bg-neutral-off-white pt-40">
         <nav className="">
           <ul className="flex flex-col gap-5">
-            <MenuItem href="/">Home</MenuItem>
-            <MenuItem href="#">New</MenuItem>
-            <MenuItem href="#">Popular</MenuItem>
-            <MenuItem href="#">Trending</MenuItem>
-            <MenuItem href="#">Categories</MenuItem>
+            <MenuItem size="2xl" href="/">
+              Home
+            </MenuItem>
+            <MenuItem size="2xl" href="#">
+              New
+            </MenuItem>
+            <MenuItem size="2xl" href="#">
+              Popular
+            </MenuItem>
+            <MenuItem size="2xl" href="#">
+              Trending
+            </MenuItem>
+            <MenuItem size="2xl" href="#">
+              Categories
+            </MenuItem>
           </ul>
         </nav>
       </div>
@@ -92,21 +135,21 @@ function App() {
           <div className="hidden md:flex">
             <nav>
               <ul className="flex text-neutral-dark-grayish-blue gap-8">
-                <li>
-                  <a href="/">Home</a>
-                </li>
-                <li>
-                  <a href="#">New</a>
-                </li>
-                <li>
-                  <a href="#">Popular</a>
-                </li>
-                <li>
-                  <a href="#">Trending</a>
-                </li>
-                <li>
-                  <a href="#">Categories</a>
-                </li>
+                <MenuItem size="base" href="/">
+                  Home
+                </MenuItem>
+                <MenuItem size="base" href="#">
+                  New
+                </MenuItem>
+                <MenuItem size="base" href="#">
+                  Popular
+                </MenuItem>
+                <MenuItem size="base" href="#">
+                  Trending
+                </MenuItem>
+                <MenuItem size="base" href="#">
+                  Categories
+                </MenuItem>
               </ul>
             </nav>
           </div>
